b7315efb89eb7282fee0d300b0af10325557bc62
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2025-01-13 00:25:27 -0500

    add projects

---
+++ src/components/experience.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Experience = ({ title, duration, company, description }) => {
+  // state that when true shows the full description, if false shows just the top
+  const [showFullDescription, setShowFullDescription] = useState(false)
+
+  return (
+    <>
+      {/* overflow-hidden trick used to force parent container to recognize the float divs */}
+      <div className="p-4 backdrop-blur-lg bg-slate-300/10 rounded-lg overflow-hidden lg:text-2xl">
+        <div className="float-left">{ title }</div>
+        <div className="float-right mx-4">{ duration }</div>
+        <br/>
+        <div class="italic text-base lg:text-lg">{company}</div>
+        <div className={`${showFullDescription ? "" : "hidden"} text-base lg:text-lg`}>
+          <ul className='list-disc list-inside'>
+            { description.map((point) => (
+              <li>{point}</li>
+            )) }
+          </ul>
+        </div>
+        <div className="flex justify-center">
+          <button onClick={ () => setShowFullDescription(
+            (prevState) => !prevState 
+          ) } className="text-2xl mr-1 hover:bg-slate-300/10 rounded-full">
+            <svg className="mt-[2px] " xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e2e8f0"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Experience
--- src/content/aboutme.md
+++ src/content/aboutme.md
@@ -1,1 +1,3 @@
-I'm currently a third year Computer Science student at the University of Waterloo and am passionate about all things software and robotics. Through multiple internships, I've had the opportunity to develop software in a variety of environments for many different projects.
+I'm currently a third year Computer Science student at the University of Waterloo and am passionate about all things software, especially machine learning and artificial intelligence. Through multiple internships, I’ve had the opportunity to develop software in a variety of environments for many projects. Currently working on turning a Kia Soul into a fully self-driving car with <a href="https://www.watonomous.ca/about" target="_blank"><u>WATOnomous</u></a>.  
+
+
