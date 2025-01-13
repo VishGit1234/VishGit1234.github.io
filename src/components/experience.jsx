6dfda5156cc2f236fbd6f361eed3c78bfe397ae2
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2025-01-13 09:20:27 -0500

    add up arrow

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -9,10 +9,12 @@ const Experience = ({ title, duration, company, description }) => {
     <>
       {/* overflow-hidden trick used to force parent container to recognize the float divs */}
       <div className="p-4 backdrop-blur-lg bg-slate-300/10 rounded-lg overflow-hidden lg:text-2xl">
-        <div className="float-left">{ title }</div>
-        <div className="float-right mx-4">{ duration }</div>
-        <br/>
-        <div class="italic text-base lg:text-lg">{company}</div>
+        <div className="overflow-hidden">
+          <div className="float-left">{ title }</div>
+          <div className="float-right mx-4">{ duration }</div>
+          <br/>
+          <div class="italic text-base lg:text-lg">{company}</div>
+        </div>
         <div className={`${showFullDescription ? "" : "hidden"} text-base lg:text-lg`}>
           <ul className='list-disc list-inside'>
             { description.map((point) => (
@@ -20,11 +22,12 @@ const Experience = ({ title, duration, company, description }) => {
             )) }
           </ul>
         </div>
-        <div className="flex justify-center">
+        <div className="mx-auto flex justify-center">
           <button onClick={ () => setShowFullDescription(
             (prevState) => !prevState 
           ) } className="text-2xl mr-1 hover:bg-slate-300/10 rounded-full">
-            <svg className="mt-[2px] " xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e2e8f0"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+            <svg className={`mt-[2px] ${showFullDescription ? "hidden" : ""}`} xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e2e8f0"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+            <svg className={`mb-[2px] ${showFullDescription ? "" : "hidden"}`} xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e2e8f0"><path d="M480-545.33 287.33-352.67 240-400l240-240 240 240-47.33 47.33L480-545.33Z"/></svg>
           </button>
         </div>
       </div>
